5b51180be6d6e9237874e36e82a72f247aa5364c
Lenovo-User <Lenovo-User@DESKTOP-TIU29D7>
2024-01-22 19:38:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+            int[] luckynumbers = { 4, 5, 8, 9 };
+            Console.WriteLine(luckynumbers[0]);
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "John";
+            friends[2] = "Peter";
+            Console.WriteLine(friends[0]);
+
+        }
+    }
+}
